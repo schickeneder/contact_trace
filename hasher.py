fcf47f4c8fa907d3bf8fd1c615d24efa112cf6db
ps <schickeneder@gmail.com>
2020-05-09 15:25:18 -0600

    Improved input/output file read/write

--- hasher.py
+++ hasher.py
@@ -1,10 +1,22 @@
-import time, hashlib
+import time, hashlib, os
 
 pattern = '%Y-%m-%d %H:%M:%S'
 
-in_files = ['20200507_capture_axon7.csv','20200507_capture_pixel3a.csv','20200507_capture_trackphone.csv']
-out_files = ['hash1_axon7', 'hash2_pixel3a', 'hash3_track']
+#in_files = ['20200507_capture_axon7.csv','20200507_capture_pixel3a.csv','20200507_capture_trackphone.csv']
+#out_files = ['hash1_axon7', 'hash2_pixel3a', 'hash3_track']
+in_files = []
+out_files = []
 
+# generate appropriate file names if none provided
+if not in_files:
+    for filename in os.listdir():
+        filename_parts = filename.split('.')
+        if filename_parts[-1].lower() == "csv":
+            print(filename)
+            in_files.append(filename)
+            out_files.append(filename_parts[0]+".hash")
+
+# generate hash for each wifi line in input file, write to output file
 for infile, outfile in zip(in_files,out_files):
     with open(infile) as f, open(outfile,"w+") as f_out:
         for line in f:
