6d86c1209448127405f28fc2e46e27daa9a96f21
ps <schickeneder@gmail.com>
2021-01-21 13:30:07 -0700

    improved, added cmdline args

--- server.py
+++ server.py
@@ -1,24 +1,28 @@
-
 import random
-from flask import Flask, jsonify, request, json
+import requests
+import sys
+from timeit import default_timer as timer
+from hashlib import sha1
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 hashes = {}
-num_bits = 32
-population_size = 1000000
-reported_carriers = 0.1*population_size
-num_hashes = int(100000)
+num_bits = 48 # overwritten by commandline arguments
+num_hashes = int(10000000) # overwritten by commandline arguments
 
 #30, 100M ~ 17.6 (15) GB total
 #30, 120M ~ 18.8 (16)
 #32, 100M ~ 18.3 (16) 
-#32, 150M ~ 20.1 (18)
+#32, 150M ~ 20.1 (18) [9.6 GB]
 #32, 190M ~ 25.4 (23)
 #32, 200M ~ 25.4 (23)
 #32, 220M ~ 26   (24)
 #32, 240M ~ 26.6
-#32, 300M ~ 28.3
+#32, 300M ~ 28.3      [19.1GB]
 
+#with gunicorn gevent, 1 worker
+#48, 300M 19.1GB ~28s for 1000*1000
+#48, 350M 20.6GB ~27s
 
 @app.route('/')
 def default():
@@ -41,11 +45,12 @@ def sim_return_matches():
 			pass
 	return jsonify(response)
 
+#https://stackoverflow.com/questions/10434599/get-the-data-received-in-a-flask-request
 @app.route('/check_contact', methods=['POST'])
 def return_matches():
 	#response = "Number of hashes is {}".format(len(hashes))
 	#this is just a temporary test
-	keylist = json.loads(request.data)
+	keylist = request.get_json()
 	#print("Request received: {}".format(keylist))
 	response = []
 
@@ -58,15 +63,58 @@ def return_matches():
 			pass
 	return jsonify(response)
 
-def main():
 
-	for i in range(1,num_hashes):
-		hashes[random.getrandbits(num_bits)] = 3 #random.randint(-90,-10)
-	print("Generated {} hashes".format(len(hashes)))
+@app.route('/auth_check_contact/<token>/<secret>', methods=['POST'])
+def auth_return_matches(token = 0, secret=0):
 
-	# may not alway print the correct number because there were some redundant hashes..
-	#print(len(hashes))
+	if (secret != sha1(token.encode()).hexdigest()):
+		return "Bad Authentication" 
 
+	keylist = request.get_json()
+	response = []
+
+	for key in keylist:
+		try:
+			#print("Trying {}".format(key))
+			response.append(hashes[int(key)])
+		except:
+			response.append(0)
+			pass
+	return jsonify(response)
+
+#this approach is too slow, instead have the user contact the key_auth to obtain token
+#then present the token to the server
+@app.route('/old_auth_check_contact/<token>', methods=['POST'])
+def old_auth_return_matches(token = 0):
+
+	if not (auth_token(token)):
+		print("Bad token")
+		return "Bad token"
+
+	keylist = request.get_json()
+	response = []
+
+	for key in keylist:
+		try:
+			#print("Trying {}".format(key))
+			response.append(hashes[int(key)])
+		except:
+			response.append(0)
+			pass
+	return jsonify(response)
+
+def random_generate_hashes(number_of_hashes):
+	start = timer()
+	for i in range(0,number_of_hashes):
+		hashes[random.getrandbits(num_bits)] = int(2e9)
+	print("Generated {}, {}-bit \"hashes\" in {} seconds.".format(len(hashes), num_bits, timer()-start))
+
+def setup(arg1,arg2):
+	global num_bits
+	global num_hashes
+	num_bits = int(arg1)
+	num_hashes = int(arg2)
+	random_generate_hashes(num_hashes)
     # try:
     #     assert (len(sys.argv) >= 2), "at least 1 argument required\n"
     #     num_hashes = str(sys.argv[1])
@@ -80,5 +128,6 @@ def main():
     #     print("Invalid syntax: " + str(error))
     #     sys.exit(-1)
 if __name__ == '__main__':
-    main()
-main()
+    setup(sys.argv[1],sys.argv[2])
+    app.run(host='0.0.0.0',port=5000, use_reloader=False)
+
